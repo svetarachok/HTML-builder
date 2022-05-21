a5ebd22181ff4e3df60efd649865fe3c6ffbda65
Sveta Rachok <sveta.r1212@gmail.com>
2022-05-21 23:47:31 +0300

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path')
+const fs = require('fs');
+const fsPromises = fs.promises;
+  
+fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
+.then( () => {
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) throw err;
+  
+    files.forEach(file => {
+      fs.readFile(path.join(__dirname, 'files', `${file}`), 'utf-8',   (err, data) => {
+        if (err) throw err;
+        fs.writeFile(path.join(__dirname, 'files-copy', `${file}`), `${data}`, ()=> {})  
+      })
+    })
+  
+  })
+    
+})
+.catch( () => {
+    console.log('Cannot create folder');
+});
+
 
